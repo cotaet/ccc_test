066cb70fc9e5f36aefedf97e1d0ee444ec036725
zuisho-1848-o <mizuki.hamada1848@gmail.com>
2023-05-13 09:37:22 +0900

    基本機能完成

--- src/gpt.ts
+++ src/gpt.ts
@@ -1,3 +1,5 @@
+import request from "request";
+import cheerio from "cheerio";
 import { Configuration, OpenAIApi } from "openai";
 
 interface TextData {
@@ -24,13 +26,19 @@ const getTextsDataFromLP = (url: string): Promise<TextData> =>  {
     });
 };
 
-const cretaePromptAfterWithURL = async (url: string) => {
+const replaceRepeatSpecialLetter = (text: string) => {
+    return text.replace(/\n+/g, "\n").replace(/\t+/g, "\t").replace(/ +/g, " ");
+}
+
+export const createPromptAfterWithURL = async (url: string) => {
     const textData = await getTextsDataFromLP(url);
-    
+    const promptAfter = `title: ${textData.title}\ndescription: ${textData.description}\ntext要素の最初の部分: ${replaceRepeatSpecialLetter(textData.texts).slice(0, 1000)}`;
+    return promptAfter;
 }
 
 
 export const accessToChatGPT = async (prompt: string) => {
+    return "ここに結果"
     const configuration = new Configuration({
         apiKey: process.env.OPENAI_API_KEY,
     });
@@ -42,4 +50,16 @@ export const accessToChatGPT = async (prompt: string) => {
         messages: [{ role: "user", content: prompt }],
     });
     return completion.data.choices[0].message?.content;
+}
+
+
+export const getBetweenJPBrackets = (text: string) => {
+    const regex = /「(.*?)」/; // 「」の中身をキャプチャする正規表現
+
+    const result = regex.exec(text);
+    if (result && result[1]) {
+        const extracted = result[1];
+        return extracted;
+    }
+    return "";
 }
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,26 @@
 import path from "path";
 import express from "express";
-import { accessToChatGPT } from "./gpt";
+import { accessToChatGPT, createPromptAfterWithURL, getBetweenJPBrackets } from "./gpt";
 
 const app = express();
 
 const port = 3000;
 
-app.public(express.static(path.join(__dirname, "public")));
+app.use((express.static(path.join(__dirname, "public"))));
+app.use(express.json());
 
 
+app.post("/gpt", async (req, res) => {
+    const body: {promptFirst: string, url: string} = req.body;
 
+    const {promptFirst, url} = body;
+    console.log(body);
 
-app.post("/create-prompt", async (req, res) => {
-    const body = req.body;
-    
-    const promptFirst = ""
-    const url = ""
+    const prompt = promptFirst + `\n\n\n` + await createPromptAfterWithURL(url);
+    const GPTResult = (await accessToChatGPT(prompt)) || "";
+    const copyResult = GPTResult.split("\n").map(text => JSON.stringify([getBetweenJPBrackets(text) || "コピー取得失敗"]));
 
-    const prompt = promptFirst + ;
-    accessToChatGPT(prompt);
+    res.json({prompt, GPTResult, copyResult});
 })
 
 
